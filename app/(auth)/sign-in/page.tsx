ca8064a7545ed054422338df45a2ea64d852e092
sandyie715 <sanjay715@infomaticscorp.com>
2025-12-04 18:58:10 +0530

    Login page and sign up page is created

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthForm from '@/components/AuthForm'
+
+const Page = () => {
+  return (
+    <div><AuthForm type="sign-in" /></div>
+  )
+}
+
+export default Page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthForm from '@/components/AuthForm'
+
+const Page = () => {
+  return (
+    <div><AuthForm type="sign-up" /></div>
+  )
+}
+
+export default Page
