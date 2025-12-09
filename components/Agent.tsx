73d7ebc8da72a0203d87a8f7c0631376f288e48c
sandyie715 <sanjay715@infomaticscorp.com>
2025-12-09 12:18:46 +0530

    Implement inteview generation

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -60,7 +60,7 @@ const Agent = ({ userName} : AgentProps) =>{
             <div className="w-full flex justify-center">
                 { callStatus !==  'ACTIVE' ? (
                     <button className="relative btn-call">
-                        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !== 'CONNECTING' & 'hidden')} />
+                        <span className={cn('absolute animate-ping rounded-full opacity-75', callStatus !== 'CONNECTING' && 'hidden')} />
                         <span>
                             {callStatus === "INACTIVE" || callStatus === 'FINISHED' ? 'Call' : '. . .'}
                         </span>
@@ -77,4 +77,4 @@ const Agent = ({ userName} : AgentProps) =>{
     )
 }
 
-export default Agent
+export default Agent;
